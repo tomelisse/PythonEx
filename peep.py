14033403c1ef8e670b0e447f0e570834ec4fd158
Maria Fabianska <tomelisse@gmail.com>
2017-08-08 14:29:55 +0200

    izip, enumerate

--- peep.py
+++ peep.py
@@ -1,6 +1,22 @@
 from itertools import chain
+from itertools import izip
 
 def peep(iterator):
     first = iterator.next()
     iterator = chain(iter([first]), iterator)
     print first, list(iterator)
+
+def my_izip(*iterables):
+    iterators = [iter(iterable) for iterable in iterables]
+    while(1):
+        t = []
+        for iterator in iterators:
+            t.append(iterator.next())
+        yield tuple(t)
+    
+    
+
+def my_enumerate(iterable):
+    indices = range(len(iterable))
+    for i, x in izip(indices, iterable):
+        yield (i,x)
